686859c5f94e233195898d3f0572e97a4fb35cab
anoopgsingh4-gif <anoopgsingh4@gmail.com>
2025-08-27 14:47:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // IMPORTANT: set base to your repo name, e.g. "/mealmate/"
 export default defineConfig({
   plugins: [react()],
-  base: "/mealmate/" // <-- change if your repo has a different name
+  base: "/MealMate/" // <-- change if your repo has a different name
 });
